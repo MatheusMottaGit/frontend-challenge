89ea82b814df1777e283f667f9be5a086239087f
Matheus Motta <matheusdomingues423@gmail.com>
2023-08-03 16:37:15 -0300

    contexts

---
+++ caputeeno/src/contexts/FilterContext.tsx
@@ -0,0 +1,22 @@
+'use client'
+import { Categories, Types } from "@/types/filter";
+import { ReactNode, createContext, useState } from "react";
+
+export const FilterContext = createContext({
+  type: Types.ALL,
+  category: Categories.NEWEST,
+  setType: (type: Types) => { },
+  setCategory: (categorie: Categories) => { }
+})
+
+export const FilterProvider = ({ children }: { children: ReactNode }) => {
+
+  const [type, setType] = useState(Types.ALL)
+  const [category, setCategory] = useState(Categories.NEWEST)
+
+  return (
+    <FilterContext.Provider value={{ type, setType, category, setCategory }}>
+      {children}
+    </FilterContext.Provider>
+  )
+}
